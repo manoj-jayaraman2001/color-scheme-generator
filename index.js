50e4bcd8b9b1f1846161309af97313e7f462c683
Manoj Jayaraman <manojjayaraman2001@gmail.com>
2023-03-01 20:59:22 +0530

    pick a color to generate color schemes, used color API

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+const dropdownEl = document.getElementById('colors')
+const getBtn = document.getElementById('get-btn')
+let colorsArray = []
+window.addEventListener('DOMContentLoaded',()=>{
+    renderColors()
+})
+getBtn.addEventListener('click', renderColors)
+
+function renderColors(){
+    let selectedColor = document.getElementById('color-picker').value
+    selectedColor = selectedColor.split('').filter((char)=>{return char!='#'}).join('')
+    let scheme = dropdownEl.value
+    
+    console.log(selectedColor)
+    let url = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${scheme}&count=5`
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            let schemeColors = data.colors
+            let colorsArray = schemeColors.map((color)=>{return color.hex.value})
+            let sectionEl = document.getElementById('section')
+            let colorsHtml = ''
+            for(hex of colorsArray){
+                colorsHtml += `
+                <div class="rectangle" id = '${hex}'>
+                    <div class="color" style = 'background: ${hex}'></div>
+                    <div class="color-hex">${hex}</div>
+                </div>`
+            }
+            sectionEl.innerHTML = colorsHtml
+
+
+            let array = document.getElementsByClassName('rectangle')
+            for(element of array){
+                element.addEventListener('click',()=>{
+                navigator.clipboard.writeText(element.id)
+                alert('copied!')
+             })
+            }
+        })
+    
+}
+
+
